c383feddccba64a7cf95fec15d50826d44434ecb
MayCV <maycvd00749@fpt.edu.vn>
2020-06-22 16:35:17 +0700

    add css

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,41 +1,32 @@
-import React, {useEffect} from 'react'
-import { connect } from 'react-redux'
-import {getNumbers} from '../../actions/addAction'
-import {Link} from 'react-router-dom'
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { getNumbers } from "../../actions/addAction";
+import { Link } from "react-router-dom";
 function Navbar(props) {
-    // console.log(props)
+  // console.log(props)
 
-    useEffect(() => {
-      getNumbers() // function from actions
-    }, [])
+  useEffect(() => {
+    getNumbers(); // function from actions
+  }, []);
 
-    return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
-          <div className="container">
-            <div className="collapse navbar-collapse" id="mobile-nav">
-              <ul className="navbar-nav mr-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to="/">
-                    Home
-                  </Link>
-                </li>
-              </ul>
-              <br/>
-              <ul className="navbar-nav mr-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to="/cart">
-                    Cart <span>{props.basketProps.basketNumbers}</span>
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-    )
+  return (
+    <div>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
+      <div className="container">
+      <button className="btn btn-default cart-icon">
+      <Link className="nav-link cart" to="/cart">
+              <span className="glyphicon glyphicon-shopping-cart"></span> <span>{props.basketProps.basketNumbers}</span>
+      </Link>
+    </button>
+      </div>
+    </nav>
+    
+    </div>
+  );
 }
 
-const mapStateToProps = state => ({
-    basketProps: state.basketState
-})
+const mapStateToProps = (state) => ({
+  basketProps: state.basketState,
+});
 
-export default connect(mapStateToProps, {getNumbers})(Navbar)
+export default connect(mapStateToProps, { getNumbers })(Navbar);
